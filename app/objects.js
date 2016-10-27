15f6244653236e9b0da3c650c2e4c514266ae180
Nic Nash <nicnash08@gmail.com>
2016-10-27 15:13:32 -0500

    objects

--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,25 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+    	obj.sayIt = fn;
+    	return obj.sayIt();
     },
 
     alterObjects : function(constructor, greeting) {
-
+    	constructor.prototype.greeting = greeting;
+    	return constructor;
     },
 
     iterate : function(obj) {
+    	let propertiesList = [];
 
+    	for (var key in obj) {
+    	    if (obj.hasOwnProperty(key)) {
+    	    	propertiesList.push(`${key}: ${obj[key]}`)
+    	  	}
+    	}
+    	
+    	return propertiesList;
     }
   };
 });
