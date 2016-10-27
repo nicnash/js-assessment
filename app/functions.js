3c3639be53dc2c0fced89c21935d405ad17904d3
Nic Nash <nicnash08@gmail.com>
2016-10-27 17:27:44 -0500

    functions 1

--- app/functions.js
+++ app/functions.js
@@ -2,24 +2,39 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define(function() {
   return {
-    argsAsArray : function(fn, arr) {
-
+    argsAsArray : function(fn,[greeting,name,punctuation]) {
+        return fn(greeting,name,punctuation);
     },
 
     speak : function(fn, obj) {
-
+        obj.speak = fn;
+        return obj.speak();
     },
 
     functionFunction : function(str) {
-
+        // console.log(str + ", ");
+        // console.log(return functionFunction(str));
     },
 
     makeClosures : function(arr, fn) {
-
+        let funcs = []
+        for (var i = 0; i < arr.length; i++) {
+            let temp = arr[i];
+            funcs.push(
+                function() {
+                    x = temp;
+                    return fn(temp);
+                });
+        }
+        return funcs;
     },
 
     partial : function(fn, str1, str2) {
-
+        return (function(punc){
+                    greeting = str1;
+                    name = str2;
+                    return fn(greeting,name,punc);
+                });
     },
 
     useArguments : function() {
@@ -39,3 +54,8 @@ define(function() {
     }
   };
 });
+
+
+
+
+['hi','hello']
