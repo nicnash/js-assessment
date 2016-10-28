c46d9b01ff05eb5648377f09341b0fcc4aa1b242
Nic Nash <nicnash08@gmail.com>
2016-10-27 22:34:11 -0500

    recursion 1 finish

--- app/recursion.js
+++ app/recursion.js
@@ -2,30 +2,31 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define(function() {
   return {
-    listFiles: function(data, dirName) {
+    listFiles: function(data, dirName,isSubfolder = false) {
     	let list = [];
     	let currdir = data.dir;
     	let files = data.files;
     	if(files){
 	    	for(const [i,fileItem] of files.entries()) {
 	    		if(dirName){
-					if(typeof fileItem === 'string' && dirName === currdir){
+	    			if(dirName === currdir){
+	    				isSubfolder = true;
+	    			}
+					if(typeof fileItem === 'string' && isSubfolder){
 						list.push(fileItem);
 					} else {
-						list = list.concat(this.listFiles(fileItem,dirName));
+						list = list.concat(this.listFiles(fileItem,dirName,isSubfolder));
 					}
 				} else {
 					if(typeof fileItem === 'string'){
 						list.push(fileItem);
 					} else {
-						list = list.concat(this.listFiles(fileItem,dirName));
+						list = list.concat(this.listFiles(fileItem,dirName,true));
 					}
 				}
 	    	}
     	}
-    	console.log(currdir,list);
     	return list;
-    	
     },
 
     permute: function(arr) {
