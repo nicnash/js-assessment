4e588dfaf4ebad8a575e2503bebdcf8948a7f22b
Nic Nash <nicnash08@gmail.com>
2016-10-28 17:43:37 -0500

    async

--- app/async.js
+++ app/async.js
@@ -3,11 +3,21 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
-
+    	// return new Promise((resolve,reject) => {
+    	// 	resolve(value);
+    	// });
+    	return Promise.resolve(value);
     },
 
     manipulateRemoteData : function(url) {
-
+		let response = fetch(url)
+			.then(data => data.json())
+			.then(data => data.people.map((item)=>item.name).sort())
+			.catch((err) => {
+				console.error(err);
+			});
+			
+		return response;
     }
   };
 });
